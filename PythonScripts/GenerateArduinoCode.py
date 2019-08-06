016e1052bcd5fc974d0f6d7bde73e8f00dfd1614
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-08-06 16:38:27 +0900

    あとはデータベース作成とボタンでどのように操作するかを書く

--- PythonScripts/GenerateArduinoCode.py
+++ PythonScripts/GenerateArduinoCode.py
@@ -11,12 +11,14 @@ def main():
         file.write(str(leftMotorStepData[i]) + ",")
     file.close()
 
-
     file = open(rightFileName, "w")
-    rightMotorStepData = np.loadtxt("RightMotor.csv", dtype=int, delimiter = ",")
+    rightMotorStepData = np.loadtxt("RightMotor.csv", dtype=int, delimiter=",")
     for i in range(len(rightMotorStepData)):
         file.write(str(rightMotorStepData[i]) + ",")
     file.close()
+
+    print("総ステップ数 : " + str(len(rightMotorStepData)))
+
 
 if __name__ == '__main__':
     main()
